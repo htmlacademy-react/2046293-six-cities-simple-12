d0e42a838b53f0e2dbd2fe766e79df1906c54b33
Evgeniy Popov <99990148+JENYAODERZHIMIY@users.noreply.github.com>
2023-03-03 15:13:13 +0300

    Разбил приложение на компоненты, исправил ошибку линтера с файлом tsconfig.json

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,12 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type MainScreenProps = {
+  numberOffers: number;
 }
 
+function App({numberOffers}: MainScreenProps): JSX.Element {
+  return (
+    <Main numberOffers={numberOffers}/>
+  );
+}
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App numberOffers={8}/>
   </React.StrictMode>,
 );
