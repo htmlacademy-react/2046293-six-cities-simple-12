1d8a5fa7a966ec89a125bd0bc01085593714a227
Evgeniy Popov <99990148+JENYAODERZHIMIY@users.noreply.github.com>
2023-03-16 14:10:41 +0300

    4.22. Обратная сторона реальности

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,22 +1,24 @@
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
-import Room from '../../pages/room/room';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const/const';
 import PageError from '../page-error/page-error';
 import PrivateRoute from '../private-route/private-route';
+import { Offers } from '../../types/offer';
+import Room from '../../pages/room/room';
 
 type AppProps = {
   numberOffers: number;
+  offers: Offers;
 }
 
-function App({numberOffers}: AppProps): JSX.Element {
+function App({numberOffers, offers}: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path={AppRoute.Main}
-          element={<Main numberOffers={numberOffers}/>}
+          element={<Main numberOffers={numberOffers} offers={offers}/>}
         />
         <Route
           path={AppRoute.Login}
---
+++ project/src/types/review.ts
@@ -0,0 +1,15 @@
+
+export type User = {
+        avatarUrl: string;
+        id: number;
+        isPro: boolean;
+        name: string;
+    }
+
+export type Review = {
+        comment: string;
+        date: string;
+        id: number;
+        rating: number;
+        user: User;
+    }
